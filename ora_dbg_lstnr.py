d6d8f4725ff9da235950fe9ae581661be2f4a0ab
Konstantin Shipkov <shipkovk@gmail.com>
2017-10-10 09:29:12 +0300

    command line tool

--- ora_dbg_lstnr.py
+++ ora_dbg_lstnr.py
@@ -1,14 +1,50 @@
 import cx_Oracle
 import time
 
-con = cx_Oracle.connect('xx/yy@zz')
-cur = con.cursor()
-v = cur.var(cx_Oracle.STRING)
-while 1==1:
-    cur.execute("""begin
-                 :p_var := zxc_dbg.read(0);
-               end;""", p_var = v )
-    print(v.getvalue())
-    time.sleep(0.1);
-con.close()
+class Lstnr():
+    def __init__(self, connect_string, pipe):
+        self.connect_string = connect_string
+        self.pipe = pipe
+    def listen(self):
+        sql = """
+  declare
+        l_message varchar2(4000) := '';
+        s varchar2(4000);
+        l_status number;
+        lf varchar2(2) := chr(10);
+  begin
+    while true loop
+        l_status := dbms_pipe.receive_message('""" + self.pipe + """', 0);
+        exit when l_status != 0;
+        dbms_pipe.unpack_message(s);
+        if length(s) + length(l_message) > 4000 then
+            raise_application_error(-20005, 'The debug message is too large!');
+        end if;
+        l_message := l_message || s || lf;
+        exit when length(l_message) > 3500;
+        lf := chr(10);
+    end loop;
+    :p_var := l_message;
+  end;
+"""
+        self.con = cx_Oracle.connect(self.connect_string)
+        cur = self.con.cursor()
+        cur.prepare(sql)
+        v = cur.var(cx_Oracle.STRING)
+        while True:
+            cur.execute(None, p_var = v )
+            if not v.getvalue() is None: print(v.getvalue(), end='')
+            time.sleep(0.1)
+        con.close()
 
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-c", "--connect", dest="connect", help="Oracle connect string")
+    parser.add_option("-p", "--pipe", dest="pipe", help="Oracle pipe name")
+    (options, args) = parser.parse_args()
+    if not options.connect:
+        parser.error("option -c (--connect) should be set")
+    if not options.pipe:
+        parser.error("option -p (--pipe) should be set")
+    Lstnr(options.connect, options.pipe).listen()
